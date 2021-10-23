3265fbc823f8db68f1b16ab2dd56718801d9755e
kako80 <kako.80@onet.eu>
2021-10-23 14:59:10 +0200

    pierwsza wersja obwód koła

---
+++ src/test2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class test2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("podaj średnicę okręgu: ");
+        float diameter = scan.nextFloat();
+        float pi = 3.14f;
+        float circuit;
+        double circouit2;
+        circuit = pi * diameter;
+        circouit2 = Math.PI * diameter;
+        System.out.println("obwód okręgu circuit wynosi: " + circuit + " obwód okręgu circuit2 wynosi: " + circouit2);
+    }
+}
