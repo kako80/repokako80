3d3946fd78b4d26b3885bdb42be1c5ca06145748
kako80 <kako.80@onet.eu>
2021-10-23 13:34:35 +0200

    druga zmodyfikowana wersja pif paf

--- src/test.java
+++ src/test.java
@@ -4,11 +4,12 @@ public class test {
     public static void main(String[] args) {
 
         for (;;) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("podaj liczbę: ");
-        int number = scan.nextInt();
+            Scanner scan = new Scanner(System.in);
+            System.out.println("podaj liczbę: ");
+            int number = scan.nextInt();
 
-            if (number >= 0)
+
+            if (number >= 0) {
                 for (int i = 0; i <= number; i++) {
                     if (i % 3 == 0 && i % 7 == 0) {
                         System.out.println("pif paf");
@@ -17,8 +18,10 @@ public class test {
                     } else if (i % 7 == 0) {
                         System.out.println("paf");
                     } else System.out.println(i);
+
                 }
-            System.out.println("niepoprawna liczba"); break;
+            } else System.out.println("liczba nieprawidłowa");
+
         }
     }
 
