312adedbba5198b5eb81e3b25d6e81fc13e29721
kako80 <kako.80@onet.eu>
2021-10-27 14:35:17 +0200

    pierwsza wersja kalkulatora

---
+++ src/calculator.java
@@ -0,0 +1,42 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args) {
+
+        String mathSign;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę działania a:");
+        float a = scan.nextFloat();
+        for (;;) {
+            System.out.println("Podaj rodzaj działania:");
+            mathSign = scan.next();
+            switch (mathSign) {
+                case "+" -> System.out.println(" + ");
+                case "-" -> System.out.println(" - ");
+                case "/" -> System.out.println(" / ");
+                case "*" -> System.out.println(" * ");
+                default -> System.out.println("Błedny znak");
+            }
+            if (!Objects.equals(mathSign, "+")&&!Objects.equals(mathSign, "-")&&!Objects.equals(mathSign, "*")&&!Objects.equals(mathSign, "/")) {
+                System.out.println();
+            } else break;
+        }
+        System.out.println("Podaj drugą liczbę działania b:");
+        float b = scan.nextFloat();
+        System.out.println("wynik działania: " + a + " " + mathSign + " " + b + " = " + calc(a, b, mathSign));
+
+    }
+    public static float calc (float a, float b, String mathSign) {
+        float v = 0;
+        switch (mathSign){
+            case "+" -> { v = a + b; }
+            case "-" -> { v = a - b; }
+            case "/" -> { v = a / b; }
+            case "*" -> { v = a * b; }
+            default -> System.out.println("Błąd");
+        } return v ;
+    }
+
+}
