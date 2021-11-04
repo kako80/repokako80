f7f343a37240d236a884f52f080e1c8eb02aff11
kako80 <kako.80@onet.eu>
2021-11-04 23:14:49 +0100

    wersja programu "szukanie duplikatów"

---
+++ src/tenBigNumbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class tenBigNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        HashSet<Integer> numbers1 = new HashSet<>();
+       int[] numbers = new int[10];
+        int i, j;
+
+        for (i = 0; i < 10; i++){
+            System.out.print("Podaj liczbę: " + (i + 1) + " ");
+             numbers[i] = scan.nextInt();
+        } scan.close();
+        for (i = 0; i < numbers.length; i++) {
+            for (j = i + 1; j < numbers.length ; j++ ) {
+                if (numbers[i] == numbers[j]) {
+                    numbers1.add(numbers[i]);
+                }
+            }
+        }
+        for (Integer integer : numbers1) {
+            System.out.print("duplikat: " + integer + "  ");
+        }
+    }
+}
